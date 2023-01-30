8f3ec0fbb8bee3fb5329a1b7beb7a77a747aeeea
sherchavira <sher.chavira@gmail.com>
2023-01-29 16:38:39 -0800

    init

---
+++ 22sumfac.py
@@ -0,0 +1,13 @@
+# 22sumfac.py
+
+# Write a program that computes the running sum from 1..n
+# Also, have it compute the factorial of n while you're at it
+# Use the same loop for both calculations
+
+# Note: you may not import math or any other library
+
+
+"""
+python3 22sumfac.py
+5 15 120
+"""
