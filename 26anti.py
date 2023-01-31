d6834f8f3a6659716c1e873af29b5359471c4278
sherchavira <sher.chavira@gmail.com>
2023-01-31 00:04:01 -0800

    new

--- 26anti.py
+++ 26anti.py
@@ -6,7 +6,19 @@
 # Variation: try this with the range() function and slice syntax
 
 dna = 'ACTGAAAAAAAAAAA'
-
+string = ''
+rdna = dna[::-1]
+for nt in rdna:
+	if nt == 'A':
+		r = 'T'
+	elif nt == 'C':
+		r = 'G'
+	elif nt == 'G':
+		r = 'C'
+	elif nt == 'T':
+		r = 'A'
+	string += r
+print(string)
 
 """
 python3 26anti.py
