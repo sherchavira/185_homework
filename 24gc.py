7b2ba96add50b29ab2edd85e24adbe1de9b1e6d9
sherchavira <sher.chavira@gmail.com>
2023-01-31 00:02:35 -0800

    new

--- 24gc.py
+++ 24gc.py
@@ -4,8 +4,11 @@
 # Format the output for 2 decimal places
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
-
-
+count = 0
+for c in dna:
+	if c == 'C' or c == 'G':
+		count += 1
+print(f"{count/len(dna):.2f}")
 """
 python3 24gc.py
 0.42
